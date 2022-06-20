21bbf010aa760b3ce78c3d7a09dd4f63d7e031e8
Nathaniel Wesley Filardo <nfilardo@microsoft.com>
2022-06-20 15:31:32 +0000

    work-bus/executor: strict unhandled rejection handling
    Terminate the task rather than let the rest of the event queue
    continue to run.

--- work-bus/executor/wrapper.sh
+++ work-bus/executor/wrapper.sh
@@ -3,5 +3,6 @@ set -e -u
 
 cd "${RUNTIME_DIRECTORY}"
 exec /usr/bin/nodejs \
+  --unhandled-rejections=strict \
   ${MORELLO_SCRIPTS}/work-bus/executor/dist/index.js \
     --config ${MORELLO_SCRIPTS}/local/executor-config.json
