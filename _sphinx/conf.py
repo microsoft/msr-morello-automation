15e913114c1a04ea9f42c7d35cb855f8914e1899
Nathaniel Wesley Filardo <nfilardo@microsoft.com>
2022-11-10 02:14:14 +0000

    sphinx: move from recommonmark to myst-parser

--- _sphinx/conf.py
+++ _sphinx/conf.py
@@ -1,7 +1,4 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
@@ -36,7 +33,7 @@ nitpicky=True
 extensions = [
   'sphinx.ext.extlinks',
   'sphinx.ext.todo',
-  'recommonmark'
+  'myst_parser'
 ]
 
 todo_include_todos = True
