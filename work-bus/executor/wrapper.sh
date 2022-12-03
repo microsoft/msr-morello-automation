3971d130f04c9064921c2981f51619647ad18483
Nathaniel Wesley Filardo <nfilardo@microsoft.com>
2022-12-03 04:55:41 +0000

    work-bus/executor: use "node" not "nodejs"
    Switching to the NodeSource nodejs PPA for a more recent nodejs version
    necessitates referring to the runtime by the former name.

--- work-bus/executor/wrapper.sh
+++ work-bus/executor/wrapper.sh
@@ -2,7 +2,7 @@
 set -e -u
 
 cd "${RUNTIME_DIRECTORY}"
-exec /usr/bin/nodejs \
+exec /usr/bin/node \
   --unhandled-rejections=strict \
   ${MORELLO_SCRIPTS}/work-bus/executor/dist/index.js \
     --config ${MORELLO_SCRIPTS}/local/executor-config.json
