842afe82f72c8f57e63a539e1b74fc726b485ad7
Nathaniel Wesley Filardo <nfilardo@microsoft.com>
2023-02-19 22:05:50 +0000

    cheribsd-exports: github runner as non-root user
    Create the user in /opt/bin/msr-morello-init (rather than requiring a
    FreeBSD host box to change /etc), drop to that user in
    /opt/bin/msr-github-act-runner.sh (to run
    msr-github-act-runner-inner.sh).
    
    We don't change the executor so that it remains compatible with our
    older root images that don't play this game, but those are not long for
    the world.

--- cheribsd-exports/files/opt/bin/msr-github-act-runner-inner.sh
+++ cheribsd-exports/files/opt/bin/msr-github-act-runner-inner.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
+# XXX
+# It's possible someone else picked up the job we were spawned for and that we
+# don't get assigned a job after all.  It would be much better if GitHub let
+# runners declare which job they wanted, but so it goes.  See
+# https://github.com/ChristopherHX/github-act-runner/issues/59 and
+# https://github.com/ChristopherHX/github-act-runner/issues/60 .  As per the
+# latter, we can time out our runner by sending it a single SIGINT: either it
+# will have picked up a job and do nothing, or it won't have picked up a job
+# yet and will stop listening and de-register itself.
+(sleep 300; kill -INT -$$) &
+
 # XXX Workaround a runtime bug
 # https://github.com/CTSRD-CHERI/cheribsd-ports/issues/9
 export GODEBUG="asyncpreemptoff=1"
 
-exec /opt/bin/github-act-runner run
+/opt/bin/github-act-runner run
+E=$?
+
+kill %1
+exit $E
--- cheribsd-exports/files/opt/bin/msr-github-act-runner.sh
+++ cheribsd-exports/files/opt/bin/msr-github-act-runner.sh
@@ -9,19 +9,4 @@ pkg64 install -y bash npm-node16
 # location.
 mv /tmp/settings.json ~worker/
 
-# XXX
-# It's possible someone else picked up the job we were spawned for and that we
-# don't get assigned a job after all.  It would be much better if GitHub let
-# runners declare which job they wanted, but so it goes.  See
-# https://github.com/ChristopherHX/github-act-runner/issues/59 and
-# https://github.com/ChristopherHX/github-act-runner/issues/60 .  As per the
-# latter, we can time out our runner by sending it a single SIGINT: either it
-# will have picked up a job and do nothing, or it won't have picked up a job
-# yet and will stop listening and de-register itself.
-(sleep 300; kill -INT -$$) &
-
-su - worker -c /opt/bin/msr-github-act-runner-inner.sh
-E=$?
-
-kill %1
-exit $E
+exec su - worker -c "exec /opt/bin/msr-github-act-runner-inner.sh"
